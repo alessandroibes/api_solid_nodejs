152e8ef43fdad81d552494c7929d4da3c6ac514f
alessandroibes <alessandro.ibes@gmail.com>
2025-07-31 15:17:49 -0300

    Implementando casos de uso: Caso de uso de perfil

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -2,29 +2,39 @@ import { User, Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class InMemoryUsersRepository implements UsersRepository {
-    public items: User[] = []
+  public items: User[] = []
 
-    async findByEmail(email: string) {
-        const user = this.items.find(item => item.email == email)
+  async findById(id: string): Promise<User | null> {
+    const user = this.items.find(item => item.id == id)
 
-        if (!user) {
-            return null
-        }
-
-        return user
+    if (!user) {
+      return null
     }
 
-    async create(data: Prisma.UserCreateInput) {
-        const user = {
-            id: 'user-1',
-            name: data.name,
-            email: data.email,
-            password_hash: data.password_hash,
-            created_at: new Date(),
-        }
+    return user
+  }
 
-        this.items.push(user)
+  async findByEmail(email: string) {
+    const user = this.items.find(item => item.email == email)
 
-        return user
+    if (!user) {
+      return null
     }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
 }
