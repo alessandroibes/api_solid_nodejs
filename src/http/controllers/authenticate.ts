d22ec901ebabf22920a93471524d65b1f00d36fe
alessandroibes <alessandro.ibes@gmail.com>
2025-08-02 17:38:30 -0300

    Controllers & Testes E2E: Implementando JWT no Fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -4,27 +4,38 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 
 export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
-    const authenticateBodySchema = z.object({
-        email: z.email(),
-        password: z.string().min(6),
+  const authenticateBodySchema = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password,
     })
 
-    const { email, password } = authenticateBodySchema.parse(request.body)
-
-    try {
-        const authenticateUseCase = makeAuthenticateUseCase()
-
-        await authenticateUseCase.execute({
-            email,
-            password,
-        })
-    } catch (err) {
-        if (err instanceof InvalidCredentialsError) {
-            return reply.status(400).send({ message: err.message })
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id
         }
+      }
+    )
 
-        throw err
+    return reply.status(200).send({
+      token
+    })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
     }
 
-    return reply.status(200).send()
+    throw err
+  }
 }
